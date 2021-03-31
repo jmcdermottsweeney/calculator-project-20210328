56872b680e4283957a6b5e094baa3747c9a4b6a2
jmcdermottsweeney <jmcdermottsweeney@gmail.com>
2021-03-30 20:40:26 -0400

    Really struggling with getting the text input and the button validation to play together nicely.

--- src/app.js
+++ src/app.js
@@ -9,28 +9,36 @@ let el = (element) => {
     return document.querySelectorAll(element);
 }
 
-let form = el('#calc__form');
-let input = el('#calc__input');
-let previousCalc = el('#calc__previous-calculation');
+let form = el('#calc__form'),
+    input = el('#calc__input'),
+    previousCalc = el('#calc__previous-calculation'),
+    numbers = el('.btn__num'),
+    operators = el('.btn__operator'),
+    allClear = el('#btn__all-clear'),
+    del = el('#btn__delete'),
+    currentOperator = undefined,
+    oldOperator = undefined,
+    newNum = undefined,
+    oldNum = undefined,
+    resultNum = 0;
 
 let formSubmit = form.addEventListener('submit', function (event) {
     event.preventDefault();
-    if (isValidInput) {
+    if (input.value.length !== 0) {
         setNum(input.value);
         seePreviousCalc();
-        console.log('New value: ' + newNum);
-        console.log('Old value: ' + oldNum);
     }
 });
 
-let numbers = el('.btn__num');
-numbers.forEach(e => e.onclick = () => input.value += String(e.innerHTML));
+numbers.forEach(e => e.onclick = () => {
+    (e.innerHTML === '.' && input.value.indexOf('.') !== -1) ? false : input.value += e.innerHTML;
+});
 
-let operators = el('.btn__operator');
-
-let newNum = undefined,
-    oldNum = undefined,
-    resultNum = 0;
+operators.forEach(e => e.onclick = () => {
+    currentOperator = e.innerHTML;
+    input.value = '';
+    console.log('Operator: ' + currentOperator);
+});
 
 let setNum = (val) => {
     if (newNum !== undefined) {
@@ -39,31 +47,63 @@ let setNum = (val) => {
     } else {
         newNum = val;
     }
+    // console.log('New value: ' + newNum);
+    // console.log('Old value: ' + oldNum);
 }
 
 let seePreviousCalc = () => {
     if (newNum !== undefined && oldNum !== undefined) {
-        previousCalc.classList.add('active');
-        previousCalc.innerHTML = oldNum + ' # ' + newNum;
+        previousCalc.innerHTML = oldNum + ' ' + currentOperator + ' ' + newNum;
+    } else if (oldNum === undefined) {
+        previousCalc.innerHTML = newNum;
+    } else {
+        previousCalc.innerHTML = '0';
     }
 }
 
 // checks that the input key is a valid number 0-9, or a decimal value
-let isValidInput = input.onkeypress = (event) => {
-    let char = (event.which) ? event.which : event.keyCode;
-    if (char === 46) {
-        if (input.value.indexOf('.') === -1) {
+let typingNumbers = input.onkeypress = (event) => {
+    let char = event.key;
+    let approvedNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+    let approvedOps = ['+', '-', '*', '/'];
+
+    approvedNums.every(e => {
+        console.log(e);
+        if (checkChar(char, e)) {
             return true;
         } return false;
-    }
-    if (char !== 46 && char > 31 && (char < 48 || char > 57)) {
-        console.log('isValidInput === false');
-        return false;
-    }
-    console.log('isValidInput === true');
-    return true;
+    });
+
+    // let char = (event.which) ? event.which : event.keyCode;
+    // if (char === 46) {
+    //     if (input.value.indexOf('.') === -1) {
+    //         return true;
+    //     } return false;
+    // }
+    // if (char !== 46 && char > 31 && (char < 48 || char > 57)) {
+    //     return false;
+    // } else {
+    //     return true;
+    // }
 }
 
-// let del = el('#btn__delete'),
-//     allClear = el('#btn__all-clear'),
-//     calculate = el('#btn__calculate');
+let checkChar = (char, filter) => {
+    if (char === filter) {
+        return true;
+    } return false;
+}
+
+allClear.onclick = () => {
+    form.reset();
+    previousCalc.innerHTML = '0';
+    newNum = undefined;
+    oldNum = undefined;
+    currentOperator = undefined;
+    oldOperator = undefined;
+}
+
+del.onclick = () => {
+    if (input.value.length !== 0) {
+        input.value = input.value.slice(0, (input.value.length - 1));
+    }
+}
