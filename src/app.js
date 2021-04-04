74f11182c4e66d34ee60cdff21e06348ac00579a
JMS <jmcdermottsweeney@jackmac.home>
2021-04-04 09:14:26 -0400

    Working 20210404

--- src/app.js
+++ src/app.js
@@ -2,25 +2,27 @@
 
 let el = (element) => {
   // If passed an id
-  if (element.charAt(0) === "#") {
+  if (element.charAt(0) === '#') {
     return document.querySelector(element);
   }
   // Otherwise get a nodelist of class
   return document.querySelectorAll(element);
 };
 
-const form = el("#calc__form"),
-  input = el("#calc__input"),
-  numbers = el(".btn__num"),
-  operators = el(".btn__operator"),
-  allClear = el("#btn__all-clear"),
-  del = el("#btn__delete"),
-  previousCalc = el("#calc__previous-calculation");
+const form = el('#calc__form'),
+  input = el('#calc__input'),
+  numbers = el('.btn__num'),
+  operators = el('.btn__operator'),
+  allClear = el('#btn__all-clear'),
+  del = el('#btn__delete'),
+  previousCalc = el('#calc__previous-calculation');
 
-let inputVal = "",
+form.addEventListener('submit', (event) => event.preventDefault());
+
+let inputVal = '',
   validOpr = new RegExp(/^[\+\-\*\/]$/),
-  currentOperator = undefined,
-  oldOperator = undefined,
+  currentOpr = undefined,
+  oldOpr = undefined,
   validNum = new RegExp(/^[\d\.]$/),
   newNum = undefined,
   oldNum = undefined,
@@ -28,63 +30,55 @@ let inputVal = "",
 
 let validateInputVal = (val) => {
   for (let i = 0; i < val.length; i++) {
-    if (
-      !validNum.test(val[i]) ||
-      (val[i] === "." && val.indexOf(".") !== -1)
-    ) {
-       val = val.slice(i,2);
-    }
     if (validOpr.test(val[i])) {
-      console.log("Detected a valid operator");
+      console.log('Valid operator detected: ' + val[i]);
+    } else if (
+      !validNum.test(val[i]) ||
+      (val[i] === '.' && val.split('.').length - 1 > 1)
+    ) {
+      console.log(val[i] + ' is not a valid character');
+      val = val.substr(0, val.length - 1);
     }
   }
-  input.value = val;
-  console.log(val);
+  console.log('Validated value is: ' + val);
+  return val;
 };
 
 // checks that the input key is a valid number 0-9, or a decimal value
 let inputNums = (input.onkeypress = (event) => {
   let char = String.fromCharCode(event.which);
   inputVal += char;
-  input.value = '';
-  validateInputVal(inputVal);
-  // if (
-  //   !validNum.test(char) ||
-  //   (char === "." && input.value.indexOf(".") !== -1)
-  // ) {
-  //   event.preventDefault();
-  // } else {
-  //   inputVal += char;
-  // }
-  // if (validOpr.test(char)) {
-  //   console.log("Detected a valid operator");
-  // }
+  console.log('Typed value is: ' + inputVal);
+  inputVal = validateInputVal(inputVal);
+  input.value = inputVal;
 });
 
 let clickNums = numbers.forEach(
   (el) =>
     (el.onclick = () => {
       inputVal += el.innerHTML;
-      validateInputVal(inputVal);
+      console.log('Clicked value is: ' + inputVal);
+      inputVal = validateInputVal(inputVal);
+      input.value = inputVal;
     })
 );
 
-let clickOprs = operators.forEach(
-  (el) =>
-    (el.onclick = () => {
-      currentOperator = el.innerHTML;
-      newNum = input.value;
-      console.log("Operator: " + currentOperator);
-    })
-);
+// let clickOprs = operators.forEach(
+//   (el) =>
+//     (el.onclick = () => {
+//       currentOpr = el.innerHTML;
+//       newNum = input.value;
+//       console.log('Operator: ' + currentOpr);
+//     })
+// );
 
 let clickAC = (allClear.onclick = () => {
   form.reset();
-  previousCalc.innerHTML = "0";
+  previousCalc.innerHTML = '0';
   newNum = undefined;
   oldNum = undefined;
-  currentOperator = undefined;
-  oldOperator = undefined;
+  currentOpr = undefined;
+  oldOpr = undefined;
 });
 
 let clickDel = (del.onclick = () => {
